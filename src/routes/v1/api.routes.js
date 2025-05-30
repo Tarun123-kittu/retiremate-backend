0cfb3b8326ca2009ee39bd5fbf97014343e16355
akriti <akshita.tomar@ultivic.com>
2025-05-30 19:27:03 +0530

    added next question api

--- src/routes/v1/api.routes.js
+++ src/routes/v1/api.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const questionnarieController = require('../../controllers/questionnaireController')
 
 router.get('/get-prime-questions',questionnarieController.getPrimeQuestions)
+router.get('/get-next-question',questionnarieController.getNextQuestion)
 
 module.exports = router
